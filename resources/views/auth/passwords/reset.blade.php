c1420988184c0661abfcb0846e945f0de2727a41
AhmedChaari <ahmed17ch@gmail.com>
2023-01-10 03:56:17 +0100

    clean S

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-
-
 <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
     <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
         <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
@@ -33,9 +31,4 @@
     </form>
     </div>
 </div>
-
-
-
-
-
 @endsection
