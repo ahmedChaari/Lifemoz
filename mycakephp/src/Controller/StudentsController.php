4927df62eb312cc9f93b9f7bf049357276533cb0
AhmedChaari <ahmed17ch@gmail.com>
2023-05-11 14:50:38 +0100

    commit end

---
+++ mycakephp/src/Controller/StudentsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+use Cake\View\View;
+
+class StudentsController extends AppController
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+
+        $this->loadModel("Students");
+        // Created app.php file inside /templates/layout/app.php
+        $this->viewBuilder()->setLayout("app");
+    }
+
+    public function addStudent()
+    {
+        $this->set("title", "Add Stduent");
+
+    }
+
+    public function listStudents()
+    {
+        $students = $this->Students->find()->toList();
+        $this->set("title", "List Student");
+        $this->set(compact("students"));
+    }
+
+    public function editStudent($id = null)
+    {
+        $student = $this->Students->get($id, [
+            'contain' => [],
+        ]);
+        $this->set(compact('student'));
+        $this->set("title", "Edit Student");
+    }
+}
